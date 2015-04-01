ccd001c0e0cf987e125b0dd6ec14881bdb9cd79f
Andrew Vaughan <asdvaughan@gmail.com>
2015-04-01 16:54:12 -0700

    Completed testing for creating and showing of a nested ticket.  Controller testing remains to be updated

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,5 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
+  validates :title, :description, presence: true
+
 end
--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -10,11 +10,11 @@ feature "Creating Tickets" do
     fill_in "Title", with: "Non-standards compliance"
     fill_in "Description", with: "My pages are ugly!"
     click_button "Create Ticket"
-    expect(page).to have_content("Ticket has been created.")
+    expect(page).to have_content("Ticket has been created")
   end
   scenario "Creating a ticket without valid attributes fails" do
     click_button "Create Ticket"
-    expect(page).to have_content("Ticket has not been created.")
+    expect(page).to have_content("Ticket has not been created")
     expect(page).to have_content("Title can't be blank")
     expect(page).to have_content("Description can't be blank")
   end
