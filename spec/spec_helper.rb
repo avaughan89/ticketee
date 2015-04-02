b45bed20804dbf0e9d76184f5a250ceec5d0ce4b
Andrew Vaughan <asdvaughan@gmail.com>
2015-04-02 08:54:14 -0700

    Removed deprecation warnings and spec helper files not in use

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,6 +25,9 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
+  config.expose_current_running_example_as :example
+  config.raise_errors_for_deprecations!
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
