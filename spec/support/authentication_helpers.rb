2ef646c4c3c961bc33d75df048a744be58310ce3
Andrew Vaughan <asdvaughan@gmail.com>
2015-04-03 10:47:36 -0700

    Completed testing of user/ticket functionality

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,4 +1,13 @@
-module AuthenticateHelpers
+module AuthenticationHelpers
   def sign_in_as!(user)
     visit '/signin'
-    fill_in "Name", with:
+    fill_in "Name", with: user.name
+    fill_in "Password", with: user.password
+    click_button 'Sign in'
+    expect(page).to have_content("Signed in successfully.")
+  end
+end
+
+RSpec.configure do |c|
+  c.include AuthenticationHelpers, type: :feature
+end
