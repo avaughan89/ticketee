4e8b69ad1dd13dfebde179050ebaf98290713295
Andrew Vaughan <asdvaughan@gmail.com>
2015-04-09 11:05:36 -0700

    Added the ability to create users from the admin backend

--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -13,7 +13,7 @@ feature "Creating Users" do
 
   scenario 'Creating a new user' do
     fill_in "Email", with: 'newbie@example.com'
-    fill_in "Passoword", with: "password"
+    fill_in "Password", with: "password"
     click_button "Create User"
     expect(page).to have_content("User has been created")
   end
