9634785273fefdc6acfaf768771468ca51171fae
Andrew Vaughan <asdvaughan@gmail.com>
2015-04-12 19:23:16 -0700

    Added permissions to users based on assigned projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :authorize_admin!, except: [:index, :show]
+  before_action :require_signin!, only: [:show]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
 
@@ -57,7 +58,7 @@ class ProjectsController < ApplicationController
   end
 
   def set_project
-    @project = Project.find(params[:id])
+    @project = Project.viewable_by(current_user).find(params[:id])
   rescue ActiveRecord::RecordNotFound
     flash[:alert]  = "The project you were looking" + " for could not be found."
 
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,12 +2,19 @@ require 'spec_helper'
 
 describe ProjectsController do
   let(:user) {FactoryGirl.create(:user)}
-
-  context 'standard users' do
     before do
       sign_in(user)
     end
 
+    it 'displays an error for a missing project' do
+    get :show, id: "not-here"
+    expect(response).to redirect_to(projects_path)
+    message = "The project you were looking for could not be found."
+
+    expect(flash[:alert]).to eql(message)
+  end
+
+  context 'standard users' do
     {
       new: :get,
       create: :post,
@@ -25,13 +32,15 @@ describe ProjectsController do
       expect(flash[:alert]).to eql("You must be an admin to do that")
     end
   end
-end
-  it 'displays an error for a missing project' do
-    get :show, id: "not-here"
-    expect(response).to redirect_to(projects_path)
-    message = "The project you were looking for could not be found."
 
-    expect(flash[:alert]).to eql(message)
+  it 'cannot access the show action without permission' do
+    project = FactoryGirl.create(:project)
+    get :show, id: project.id
+
+    expect(response).to redirect_to(projects_path)
+    expect(flash[:alert]).to eql("The project you were looking " + "for could not be found")
   end
+end
+
 
 end
