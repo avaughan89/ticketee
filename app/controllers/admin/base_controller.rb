3c36e166195e0429c447672254e615aaaa41c226
Andrew Vaughan <asdvaughan@gmail.com>
2015-04-14 16:37:28 -0700

    Updated admin base controller with corrected authorize admin function

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,4 +3,15 @@ class Admin::BaseController < ApplicationController
 
   def index
   end
+
+  private
+
+  def authorize_admin!
+    authenticate_user!
+
+    unless current_user.admin?
+      flash[:alert] = 'You must be an admin to do that.'
+      redirect_to root_path
+    end
+  end
 end
