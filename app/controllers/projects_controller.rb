b263c5b1d9b710bea7fa5c39fe459802397d8e95
Andrew Vaughan <asdvaughan@gmail.com>
2015-04-19 15:07:45 -0700

    Updated projects controller for new variable names and helpers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,11 @@
 class ProjectsController < ApplicationController
-  before_action :authorize_admin!, except: [:index, :show]
-  before_action :require_signin!, only: [:index, :show]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-
   def index
-    @projects = Project.for(current_user)
+    @projects = Project.all
+  end
+
+  def show
   end
 
   def new
@@ -16,54 +16,47 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
 
     if @project.save
-      redirect_to @project, notice: "Project has been created."
+      flash[:notice] = "Project has been created."
 
+      redirect_to @project
     else
-      flash[:alert] =  "Project has not been created"
-      render 'new'
+      flash[:alert] = "Project has not been created."
+
+      render "new"
     end
   end
 
-  def show
-
-  end
-
   def edit
-
   end
 
   def update
-
     if @project.update(project_params)
-
-      flash[:notice] = "Project has been updated"
+      flash[:notice] = "Project has been updated."
       redirect_to @project
     else
-      flash[:alert] = "Project has not been updated"
-      render 'edit'
+      flash[:alert] = "Project has not been updated."
+      render "edit"
     end
   end
 
   def destroy
-
     @project.destroy
 
-    redirect_to projects_path, notice: "Project has been destroyed"
-  end
-
-  private
-
-  def project_params
-    params.require(:project).permit(:name, :description)
-  end
-
-  def set_project
-    @project = Project.for(current_user).find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    flash[:alert]  = "The project you were looking" + " for could not be found"
+    flash[:notice] = "Project has been destroyed."
 
     redirect_to projects_path
   end
 
+  private
 
+    def project_params
+      params.require(:project).permit(:name, :description)
+    end
+
+    def set_project
+      @project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking for could not be found."
+      redirect_to projects_path
+    end
 end
